936dd0f3fe1795c884ac64a94726d167a096a7f1
wangyufeng <xiaomimang@163.com>
2023-09-12 14:41:52 +0800

    fix:更新

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
-conda env create -f environment.yml
-CALL conda.bat activate base
+@REM 停止当前运行的conda环境
+@REM CALL conda.bat deactivate
+@REM conda env remove --name base
+@REM conda env create -f environment.yml
+@REM CALL conda.bat activate base
 pip install -r requirements.txt -i https://pypi.douban.com/simple
 cd ./testCase
 pytest
