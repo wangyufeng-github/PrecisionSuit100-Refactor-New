4c0bf993dd9821a6fb8bacaa90ae246bf4644803
wangyufeng <xiaomimang@163.com>
2023-09-12 11:22:07 +0800

    fix:

--- run.bat
+++ run.bat
@@ -1,7 +1,4 @@
-python -m venv venv
-Set-ExecutionPolicy RemoteSigned
-venv\Scripts\Activate
 pip install -r requirements.txt -i https://pypi.douban.com/simple
 cd ./testCase
 pytest
-deactivate
+pause
